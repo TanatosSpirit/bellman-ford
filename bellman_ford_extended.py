6bde643fd55e90868a3eb2960a48837782ba17b0
Andrei Liutov <lytovnadrei@gmail.com>
2019-11-06 19:27:30 +0300

    upgrade

--- bellman_ford_extended.py
+++ bellman_ford_extended.py
@@ -7,14 +7,19 @@ from collections import defaultdict
 # Class to represent a graph
 class Graph:
 
-    def __init__(self, vertices):
-        self.V = vertices  # No. of vertices
+    def __init__(self):
+    # def __init__(self, vertices):
+        self.V = 0  # No. of vertices
         self.graph = []  # default dictionary to store graph
+        self.predecessor = [-1]
+
+    def set_num_vertices(self, vertices):
+        self.V = vertices
         self.predecessor = [-1] * vertices
 
     # function to add an edge to graph
-    def addEdge(self, u, v, w):
-        self.graph.append([u, v, w])
+    def addEdge(self, u, v, w, a):
+        self.graph.append([u, v, w, a])
 
     # utility function used to print the solution
     def printArr(self, dist):
@@ -44,20 +49,22 @@ class Graph:
             # Update dist value and parent index of the adjacent vertices of
             # the picked vertex. Consider only those vertices which are still in
             # queue
-            for u, v, w in self.graph:
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-                    self.predecessor[v] = u
+            for u, v, w, a in self.graph:
+                if a == 1:
+                    if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+                        self.predecessor[v] = u
 
                 # Step 3: check for negative-weight cycles. The above step
         # guarantees shortest distances if graph doesn't contain
         # negative weight cycle. If we get a shorter path, then there
         # is a cycle.
 
-        for u, v, w in self.graph:
-            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
+        for u, v, w, a in self.graph:
+            if a == 1:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                    print("Graph contains negative weight cycle")
+                    return
 
         # print all distance
         # self.printArr(dist)
@@ -67,15 +74,16 @@ class Graph:
 
 def main():
     # Граф для тестов
-    g = Graph(4)
-    g.addEdge(0, 1, 8)
-    g.addEdge(1, 2, 5)
-    g.addEdge(2, 3, 8)
-    g.addEdge(2, 0, 9)
-    g.addEdge(3, 0, 5)
+    g = Graph()
+    g.set_num_vertices(4)
+    g.addEdge(0, 1, 8, 1)
+    g.addEdge(1, 2, 5, 1)
+    g.addEdge(2, 3, 8, 1)
+    g.addEdge(2, 0, 9, 1)
+    g.addEdge(3, 0, 5, 1)
 
     # Print the solution
-    print(g.BellmanFord(2))
+    print(g.BellmanFord(0))
 
 
 if __name__ == '__main__':
